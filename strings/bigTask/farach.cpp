62163053bfee003f3a4a51236ad35829f80cdf6e
Stepan Kargaltsev <stepan.kargaltsev@phystech.edu>
2015-11-23 16:42:58 +0300

    even tree

--- strings/bigTask/farach.cpp
+++ strings/bigTask/farach.cpp
@@ -4,6 +4,7 @@
 #include <utility>
 #include <algorithm>
 #include <climits>
+#include <cassert>
 
 using std::vector;
 using std::make_pair;
@@ -13,23 +14,11 @@ using std::pair;
 
 struct SuffixTree {
     struct Node {
-        struct Transition {
-            unsigned int symbol;
-            unsigned int nextIndex;
-            Transition(unsigned int symbol, unsigned int nextIndex) : 
-                symbol(symbol), 
-                nextIndex(nextIndex) {
-            }
-        };
         int parent;
-        unsigned int depth;
+        unsigned int lastIndex;
         unsigned int indexOfParentEdge;
         int leaf;
-        vector <Transition> children;
-        
-        void addTransition(unsigned int symbol, unsigned int nextIndex) {
-            children.push_back(Transition(symbol, newIndex));
-        }
+        vector <unsigned int> children;
     };
     
     vector <Node> nodes;
@@ -42,16 +31,17 @@ struct SuffixTree {
         root = 0;
         Node &rootNode = nodes[root];
         rootNode.parent = -1;
-        rootNode.depth = 0;
+        rootNode.lastIndex = 0;
         rootNode.indexOfParentEdge = 0;
         rootNode.leaf = 0;
     }
     
-    void deleteNode(unsigned int index) {
+    void deleteNode(unsigned int index, vector <unsigned int> &newParentsChildren) {
         Node &node = nodes[index];
         for (auto const &u: node.children) {
             nodes[u].parent = node.parent;
             nodes[u].indexOfParentEdge = node.indexOfParentEdge;
+            newParentsChildren.push_back(u);
         }
         pull.push_back(index);
     }
@@ -100,45 +90,81 @@ struct NumberedPair {
     }
 };
 
-void decompressDfs(unsigned int v, vector <SuffixTree::Node::Transition> &parentsNewChildren,
+void decompressDfs(unsigned int v, vector <unsigned int> &parentsNewChildren,
                    SuffixTree &compressed, const vector <int> &input) {
-    vector <SuffixTree::Node::Transition> myNewChildren;
+    vector <unsigned int> myNewChildren;
     SuffixTree::Node &currentNode = compressed.nodes[v];
     for (auto const &u: currentNode.children) {
-        decompressDfs(u.nextIndex, myNewChildren, compressed, length);
+        decompressDfs(u, myNewChildren, compressed, input);
     }
     currentNode.children = myNewChildren;
+    myNewChildren.clear();
     
-    currentNode.depth = min(currentNode.depth * 2, input.size());
+    currentNode.lastIndex = min(currentNode.lastIndex * 2, static_cast<unsigned int>(input.size()));
     currentNode.indexOfParentEdge *= 2;
     if (currentNode.leaf != -1) {
         currentNode.leaf *= 2;
     }
-    if (!currentNode.children.size()) {
-        return;
-    }
     
-    vector <unsigned int> similarKids;
-    similarKids.push_back(0);
-    for (unsigned int i = 1; i < currentNode.children.size(); ++i) {
-        unsigned int current = currentNode.children[i].nextIndex;
-        unsigned int previous = currentNode.children[i - 1].nextIndex;
-        while (input[compressed.nodes[current].indexOfParentEdge * 2] ==
-            input[compressed.nodes[previous].indexOfParentEdge * 2]) {
-            ++i;
-            unsigned int previous = current;
-            unsigned int current = currentNode.children[i].nextIndex;
+    if (currentNode.children.size()) {
+        vector <unsigned int> similarKids;
+        similarKids.push_back(currentNode.children[0]);
+        for (unsigned int i = 1; i <= currentNode.children.size(); ++i) {
+            if (i != currentNode.children.size()) {
+                unsigned int current = currentNode.children[i];
+                unsigned int previous = currentNode.children[i - 1];
+                while (i < currentNode.children.size() && 
+                    input[compressed.nodes[current].indexOfParentEdge] ==
+                    input[compressed.nodes[previous].indexOfParentEdge]) {
+                    similarKids.push_back(current);
+                    ++i;
+                    previous = current;
+                    if (i < currentNode.children.size()) {
+                        current = currentNode.children[i];
+                    }
+                }
+            }
+            if (similarKids.size() != 1) {
+                unsigned int newNodeIndex = compressed.newNode();
+                currentNode = compressed.nodes[v];
+                auto &newNode = compressed.nodes[newNodeIndex];
+                newNode.parent = v;
+                newNode.indexOfParentEdge = compressed.nodes[similarKids.back()].indexOfParentEdge;
+                newNode.lastIndex = newNode.indexOfParentEdge + 1;
+                newNode.children = similarKids;
+                newNode.leaf = -1;
+                for (auto &u: similarKids) {
+                    auto &currentChild = compressed.nodes[u];
+                    currentChild.parent = newNodeIndex;
+                    ++currentChild.indexOfParentEdge;
+                    if (currentChild.indexOfParentEdge == currentChild.lastIndex) {
+                        assert(currentChild.children.size() == 0);
+                        newNode.leaf = currentChild.leaf;
+                        compressed.deleteNode(u, newNode.children);
+                    }
+                }
+                myNewChildren.push_back(newNodeIndex);
+            } else {
+                myNewChildren.push_back(similarKids.back());
+            }
+            if (i != currentNode.children.size()) {
+                similarKids.clear();
+                similarKids.push_back(currentNode.children[i]);
+            }
         }
-        unsigned int newNodeIndex = compressed.newNode();
-        auto &newNode = compressed.nodes[newNodeIndex];
-        similarKids.push_back(i);
     }
-    
-    
+    currentNode.children = myNewChildren;
+    if (myNewChildren.size() == 1 && v != compressed.root) {
+        compressed.deleteNode(v, parentsNewChildren);
+    } else {
+        parentsNewChildren.push_back(v);
+    }
 }
 
 void decompress(SuffixTree &compressed, const vector <int> &input) {
-    decompressDfs(compressed.root, compressed, input);
+    vector <unsigned int> tmp;
+    decompressDfs(compressed.root, tmp, compressed, input);
+    assert(tmp.size() == 1 && tmp[0] == compressed.root);
 }
 
 SuffixTree buildSuffixTree(const vector <int> &input) {
@@ -150,10 +176,10 @@ SuffixTree buildSuffixTree(const vector <int> &input) {
         unsigned int newNodeIndex = result.newNode();
         SuffixTree::Node &newNode = result.nodes[newNodeIndex];
         newNode.parent = result.root;
-        newNode.depth = 1;
+        newNode.lastIndex = 1;
         newNode.indexOfParentEdge = 0;
         newNode.leaf = 0;
-        result.nodes[result.root].addTransition(input[0], newIndex);
+        result.nodes[result.root].children.push_back(newNodeIndex);
         result.nodes[result.root].leaf = 1;
         return result;
     }
@@ -190,10 +216,94 @@ SuffixTree buildSuffixTree(const vector <int> &input) {
     return SuffixTree();
 }
 
+
+void dfs_(SuffixTree &tree, int v, int count) {
+    for (int i = 0; i < count; ++i) {
+        printf("-");
+    }
+    printf(" %d: %d %d %d %d", v, tree.nodes[v].parent, tree.nodes[v].indexOfParentEdge, tree.nodes[v].lastIndex, tree.nodes[v].leaf);
+    for (auto const &u: tree.nodes[v].children) {
+        dfs_(tree, u, count + 1);
+    }
+}
+
+void sample_() {
+    SuffixTree sample;
+    int newNode = sample.newNode();
+    sample.nodes[newNode].parent = sample.root;
+    sample.nodes[newNode].indexOfParentEdge = 1;
+    sample.nodes[newNode].lastIndex = 6;
+    sample.nodes[newNode].leaf = 1;
+    sample.nodes[sample.root].children.push_back(newNode);
+    
+    
+    newNode = sample.newNode();
+    sample.nodes[newNode].parent = sample.root;
+    sample.nodes[newNode].indexOfParentEdge = 0;
+    sample.nodes[newNode].lastIndex = 1;
+    sample.nodes[newNode].leaf = -1;
+    sample.nodes[sample.root].children.push_back(newNode);
+    int root = newNode;
+    
+    newNode = sample.newNode();
+    sample.nodes[newNode].parent = root;
+    sample.nodes[newNode].indexOfParentEdge = 1;
+    sample.nodes[newNode].lastIndex = 6;
+    sample.nodes[newNode].leaf = 0;
+    sample.nodes[root].children.push_back(newNode);
+    
+    newNode = sample.newNode();
+    sample.nodes[newNode].parent = root;
+    sample.nodes[newNode].indexOfParentEdge = 3;
+    sample.nodes[newNode].lastIndex = 6;
+    sample.nodes[newNode].leaf = 2;
+    sample.nodes[root].children.push_back(newNode);
+    
+    
+    newNode = sample.newNode();
+    sample.nodes[newNode].parent = sample.root;
+    sample.nodes[newNode].indexOfParentEdge = 5;
+    sample.nodes[newNode].lastIndex = 6;
+    sample.nodes[newNode].leaf = 5;
+    sample.nodes[sample.root].children.push_back(newNode);
+    
+    newNode = sample.newNode();
+    sample.nodes[newNode].parent = sample.root;
+    sample.nodes[newNode].indexOfParentEdge = 3;
+    sample.nodes[newNode].lastIndex = 6;
+    sample.nodes[newNode].leaf = 3;
+    sample.nodes[sample.root].children.push_back(newNode);
+    
+    
+    newNode = sample.newNode();
+    sample.nodes[newNode].parent = sample.root;
+    sample.nodes[newNode].indexOfParentEdge = 4;
+    sample.nodes[newNode].lastIndex = 6;
+    sample.nodes[newNode].leaf = 4;
+    sample.nodes[sample.root].children.push_back(newNode);
+    
+    vector <int> input;
+    input.push_back(1);
+    input.push_back(2);
+    input.push_back(1);
+    input.push_back(1);
+    input.push_back(1);
+    input.push_back(2);
+    input.push_back(2);
+    input.push_back(1);
+    input.push_back(2);
+    input.push_back(2);
+    input.push_back(2);
+    decompress(sample, input);
+    dfs_(sample, sample.root, 0);
+}
+
+
 int main() {
+    sample_();
     int n;
     scanf("%d", &n);
-    vector <unsigned int> x(n);
+    vector <int> x(n);
     for (int i = 0; i < n; ++i)
         scanf("%d", &x[i]);
     buildSuffixTree(x);
